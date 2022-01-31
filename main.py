9af6e6095f2c4cb0e2fa44c9cbf565e0ce13e1ea
ADM228 <msh.alexgoog@gmail.com>
2022-01-31 20:44:50 +0300

    Icon + window title adjustments + menu added

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from PyQt5 import QtWidgets, uic, QtGui
+version = "0.1.0"
 app = QtWidgets.QApplication([])
 
 ui = uic.loadUi("Ui/Main.ui")
 ui.setWindowIcon(QtGui.QIcon('icon.ico'))
+ui.setWindowTitle("M7TE v"+version)
 
 ui.show()
 app.exec()
