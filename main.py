7832ca97a8106b7d301ce61628df6fc688295b65
ADM228 <msh.alexgoog@gmail.com>
2022-02-01 13:39:14 +0300

    Everything is class-based now

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
 from PyQt5 import QtWidgets, uic, QtGui
-version = "0.1.0"
-app = QtWidgets.QApplication([])
+import sys, os, json, configparser
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.version="1.0.0"
+        self.ui = uic.loadUi("Ui/Main.ui")
+        self.ui.setWindowIcon(QtGui.QIcon('icon.ico'))
+        self.ui.setWindowTitle("M7TE v"+self.version+"α")
+        self.parseSettings()
+        self.localize("Alex")
+        self.ui.show()
+    def localize(self,lang):
+        pass
+    def parseSettings(self):
+        pass
 
-ui = uic.loadUi("Ui/Main.ui")
-ui.setWindowIcon(QtGui.QIcon('icon.ico'))
-ui.setWindowTitle("M7TE v"+version)
+Lang = """
+[en_us]:
+menu.file = File
 
-ui.show()
-app.exec()
+"""
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    window = MainWindow()
+    sys.exit(app.exec_())
